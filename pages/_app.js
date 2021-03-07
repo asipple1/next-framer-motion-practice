94990837c11fefff19f0c1921ed93de9f6d68e3b
AndrewS <andrews@Andy.local>
2021-03-06 15:00:33 -1000

    Add wrapper animate

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,27 @@
 import '../styles/globals.css'
-import { AnimatePresence } from "framer-motion";
+import { AnimatePresence, motion } from "framer-motion";
+
+
+const fadeInOut = {
+  initial: {
+    opacity: 0,
+  },
+  animate: {
+    opacity: 1,
+  },
+  exit: {
+    opacity: 0,
+  }
+};
+
 
 function MyApp({ Component, pageProps, router }) {
   return (
     <AnimatePresence exitBeforeEnter>
-      <Component {...pageProps} key={router.route} />
-    </AnimatePresence>
+      <motion.div key={router.route} variants={fadeInOut} initial="initial" animate="animate">
+        <Component {...pageProps} />
+      </motion.div>
+  </AnimatePresence>
   )
 }
 
